f0baa6249e283b7c6ade2d63d7c833b09797c968
disor <disorn.bootso@gmail.com>
2023-09-14 22:09:37 +0700

    Change .proto Branch Service

--- client/index.js
+++ client/index.js
@@ -6,14 +6,19 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-// app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "hbs");
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  client.getAllBranch(null, (err, data) => {
+  client.getAll(null, (err, data) => {
+    if (!err) {
+      res.json(data);
+    }
+  });
+});
+
+app.get("/:id", (req, res) => {
+  client.get({ id: req.params.id }, (err, data) => {
     if (!err) {
       res.json(data);
     }
@@ -26,12 +31,12 @@ app.post("/save", (req, res) => {
     address: req.body.address,
     telNum: req.body.telNum,
   };
+
   client.insert(newBranchItem, (err, data) => {
     if (err) throw err;
 
     console.log("New Branch created successfully", data);
-    // res.json(data);
-    res.redirect("/");
+    res.sendStatus(200);
   });
 });
 
@@ -42,36 +47,24 @@ app.post("/update", (req, res) => {
     address: req.body.address,
     telNum: req.body.telNum,
   };
-  console.log(
-    "update Item %s %s %d",
-    updateBranchItem.id,
-    req.body.name,
-    req.body.address,
-    req.body.telNum
-  );
 
   client.update(updateBranchItem, (err, data) => {
     if (err) throw err;
 
     console.log("Branch Item updated successfully", data);
     res.json(data);
-    res.redirect("/");
   });
 });
 
-app.post("/remove", (req, res) => {
-  client.remove({ id: req.body.id }, (err, _) => {
+app.delete("/remove/:id", (req, res) => {
+  client.remove({ id: req.params.id }, (err, _) => {
     if (err) throw err;
     console.log("Branch Item removed successfully");
-    res.redirect("/");
+    res.sendStatus(200);
   });
 });
 
-const PORT = process.env.PORT || 3000;
-// app.use((err, req, res, next) => {
-//   console.error(err.stack);
-//   res.status(500).send("Something broke!");
-// });
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log("Server running at port %d", PORT);
